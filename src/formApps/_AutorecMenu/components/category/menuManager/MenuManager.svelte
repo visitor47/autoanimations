2b4a3b85a96f0bc8103d2ef0b6003d80b385caf6
theripper93 <tsplab@gmail.com>
2025-01-03 22:08:37 +0100

    a

--- src/formApps/_AutorecMenu/components/category/menuManager/MenuManager.svelte
+++ src/formApps/_AutorecMenu/components/category/menuManager/MenuManager.svelte
@@ -49,7 +49,7 @@
                 modal: true,
                 onOk: (app) => {
                     //@ts-ignore
-                    const form = app.element.find("form")[0];
+                    const form = app.application.element.find("form")[0];
                     if (!form.data.files.length)
                         return ui.notifications?.error(
                             "You did not upload a data file!"
