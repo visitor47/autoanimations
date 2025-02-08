ea9b6bc7392327e60e2033139b60128b4dafbafd
Tim L. White <tim@cyface.com>
2025-02-08 09:45:01 -0700

    Update aa-dcc.js

--- src/system-support/aa-dcc.js
+++ src/system-support/aa-dcc.js
@@ -3,12 +3,13 @@ import AAHandler            from "../system-handlers/workflow-data.js";
 import { getRequiredData }  from "./getRequiredData.js";
 
 export function systemHooks() {
-    Hooks.on("createChatMessage", async (msg) => {
+   Hooks.on("createChatMessage", async (msg) => {
     if (msg.author.id !== game.user.id) {
       return;
     }
+    const itemId = msg.system?.weaponId || msg.system?.spellId || msg.system?.skillId || msg.flags?.dcc?.ItemId;
     let compiledData = await getRequiredData({
-      itemId: msg.system?.weaponId,
+      itemId,
       actorId: msg.speaker?.actor,
       tokenId: msg.speaker?.token,
       workflow: msg
