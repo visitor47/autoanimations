1712bbb6a4b51946ae0804e249ea7ea3904658f3
Tim L. White <tim@cyface.com>
2025-02-08 09:29:18 -0700

    Update aa-dcc.js

--- src/system-support/aa-dcc.js
+++ src/system-support/aa-dcc.js
@@ -4,25 +4,20 @@ import { getRequiredData }  from "./getRequiredData.js";
 
 export function systemHooks() {
     Hooks.on("createChatMessage", async (msg) => {
-        if (msg.user.id !== game.user.id) { return };
-
-        let compiledData = await getRequiredData({
-            itemId: msg.flags?.dcc?.ItemId,
-            actorId: msg.speaker?.actor,
-            tokenId: msg.speaker?.token,
-            workflow: msg,
-        })
-        runDcc(compiledData)
+    if (msg.author.id !== game.user.id) {
+      return;
+    }
+    let compiledData = await getRequiredData({
+      itemId: msg.system?.weaponId,
+      actorId: msg.speaker?.actor,
+      tokenId: msg.speaker?.token,
+      workflow: msg
     });
+    runDcc(compiledData);
+  });
 }
 
 async function runDcc(input) {
-
-    if (!game.settings.get('dcc', 'useStandardDiceRoller')) {
-        const handler = await AAHandler.make(input)
-        trafficCop(handler);
-    } else if (input.flags?.dcc?.RollType === "Damage" || input.flags?.dcc?.RollType === "SpellCheck") {
-        const handler = await AAHandler.make(input)
-        trafficCop(handler);
-    }
+  const handler = await AAHandler.make(input)
+  trafficCop(handler);
 }
